9ceb0e66bf2b4c95ff89fc1e11c95470213317cd
Anton Bialenik <tony.baton@yandex.by>
2018-09-19 10:59:39 +0300

    first

---
+++ WebApplication5/QuestionController.js
@@ -0,0 +1,37 @@
+﻿var myApp = angular.module('myApp');
+myApp.controller('QuestionController', function($scope) {
+
+    $scope.question = {
+        text: 'Какой js-фреймворк лучше использовать?',
+        author: 'Иван Иванов',
+        date: '20.10.2013',
+        answers: [
+            {
+                text: 'AngularJS',
+                author: 'Вова Сидор',
+                date: '20.10.2013',
+                rate: 2
+            }, {
+                text: 'Angular Js the best',
+                author: 'Витя Пчелкин',
+                date: '20.10.2013',
+                rate: 0
+            }, {
+                text: 'Я бы использовал knockout',
+                author: 'Неизвестный',
+                date: '21.10.2013',
+                rate: 0
+            }
+        ]
+    };
+    $scope.voteUP = function(answear) {
+        answear.rate++;
+    };
+    $scope.voteDown = function(answear) {
+        answear.rate--;
+    };
+    $scope.questColorClass = "questcolor";
+    $scope.changeClass = function(e) {
+        $scope.questColorClass = e.type == "mouseover" ? "questselectedcolor" : "questcolor";
+    }
+});
