682b710dc280b100445fcafa25a2c2d4721c90e4
Anton Bialenik <tony.baton@yandex.by>
2018-09-20 11:50:55 +0300

    second commit

--- WebApplication5/QuestionController.js
+++ WebApplication5/QuestionController.js
@@ -24,14 +24,16 @@ myApp.controller('QuestionController', function($scope) {
             }
         ]
     };
-    $scope.voteUP = function(answear) {
-        answear.rate++;
+    $scope.voteUp = function(answer) {
+        answer.rate++;
     };
-    $scope.voteDown = function(answear) {
-        answear.rate--;
+    $scope.voteDown = function(answer) {
+        answer.rate--;
     };
     $scope.questColorClass = "questcolor";
     $scope.changeClass = function(e) {
         $scope.questColorClass = e.type == "mouseover" ? "questselectedcolor" : "questcolor";
     }
+    $scope.answerColorClass = "answcolor";
+
 });
